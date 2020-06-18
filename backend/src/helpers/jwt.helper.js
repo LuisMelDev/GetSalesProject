c62fe389c251277ac70db9d07570799714dcf1da
exroot <eliastorrealba1998@outlook.com>
2020-06-17 20:30:46 -0400

    adding helpers

--- backend/src/helpers/jwt.helper.js
+++ backend/src/helpers/jwt.helper.js
@@ -1,6 +1,6 @@
-const { sign } = require('jsonwebtoken')
-const { JWT_SECRET } = require('../config/configApp')
+const { sign } = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/configApp");
 
-module.exports.generateToken = function(user){
-    return sign({user},JWT_SECRET,{ expiresIn: "2h" })
-}
+module.exports.generateToken = (user) => {
+    return sign({ user }, JWT_SECRET, { expiresIn: "2h" });
+};
