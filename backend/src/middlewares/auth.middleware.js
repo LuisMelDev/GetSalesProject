ffed267964e1a43f30d7d12955c5752c3929a22c
exroot <eliastorrealba1998@outlook.com>
2020-06-17 20:27:40 -0400

    adding middlewares

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,3 +1,19 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/configApp");
+const ErrorHelper = require("../helpers/error.helper");
+
 module.exports = (req, res, next) => {
-    next();
+    const token = req.headers["authorization"];
+    if (!token) {
+        ErrorHelper(400, "Token must be sent");
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+            ErrorHelper(401, "Invalid token");
+        }
+
+        req.user = decodedToken.user;
+        next();
+    });
 };
