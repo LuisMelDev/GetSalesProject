d9c02a9893e0e718590b4f26cd1ec24ccc63f314
Luis Melendez <meelendez12341@gmail.com>
2020-06-19 22:44:17 -0430

    deleted usuario page

--- frontend/src/app/modulos/administracion/administracion-routing.module.ts
+++ frontend/src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrincipalComponent } from './pages/principal/principal.component';
-import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 import { ReportesComponent } from './pages/reportes/reportes.component';
 import { BitacoraComponent } from './pages/bitacora/bitacora.component';
 
 
 const routes: Routes = [
   {path:'administracion', component:PrincipalComponent, children: [
-    {path: 'usuarios', component:UsuariosComponent },
     {path: 'reportes', component:ReportesComponent },
     {path: 'bitacora', component:BitacoraComponent },
   ]}
--- frontend/src/app/modulos/administracion/administracion.module.ts
+++ frontend/src/app/modulos/administracion/administracion.module.ts
@@ -7,21 +7,18 @@ import { ComponentsModule } from 'src/app/components/components.module';
 
 import { AdministracionRoutingModule } from './administracion-routing.module';
 import { PrincipalComponent } from './pages/principal/principal.component';
-import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 import { ReportesComponent } from './pages/reportes/reportes.component';
 import { BitacoraComponent } from './pages/bitacora/bitacora.component';
 
 @NgModule({
   declarations: [
     PrincipalComponent,
-    UsuariosComponent,
     ReportesComponent,
     BitacoraComponent
   ],
   imports: [CommonModule, AdministracionRoutingModule, ComponentsModule],
   exports: [
     PrincipalComponent,
-    UsuariosComponent,
     ReportesComponent,
     BitacoraComponent,
   ],
