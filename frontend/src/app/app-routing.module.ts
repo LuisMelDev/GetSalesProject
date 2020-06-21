3024e29435ca6af635ad6b7174ee78774642c0b5
Hacche <1gatubela1@gmail.com>
2020-06-20 20:04:21 -0430

    Añadiendo rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { ReportesComponent } from './pages/reportes/reportes.component';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 import { ClientesComponent } from './pages/clientes/clientes.component';
+import { AgregarUsuariosComponent } from './pages/usuarios/agregar/agregar.component';
+import { AgregarClientesComponent } from './pages/cliente/agregar/agregar.component';
+import { ActualizarUsuariosComponent } from './pages/usuarios/actualizar/actualizar.component';
+import { ActualizarClientesComponent } from './pages/cliente/actualizar/actualizar.component';
 
 
 
@@ -19,8 +23,10 @@ const routes: Routes = [
   {path: 'productos', component: ProductosComponent},
   {path: 'usuarios', component: UsuariosComponent},
   {path: 'clientes', component: ClientesComponent},
-  
-  
+  {path: 'usuarios/agregar', component: AgregarUsuariosComponent },
+  {path: 'usuarios/actualizar', component: ActualizarUsuariosComponent },
+  {path: 'clientes/agregar', component: AgregarClientesComponent }, 
+  {path: 'clientes/actualizar', component: ActualizarClientesComponent }
 ];
 
 @NgModule({
